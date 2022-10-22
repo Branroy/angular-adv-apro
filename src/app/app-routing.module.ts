b7541a621e88b5eac89cba8d2ef75df63f9b00c9
Roy Ortiz <branroy14@gmail.com>
2022-10-21 19:13:16 -0500

    Fin sección 4 - Router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,36 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes} from '@angular/router'
 
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { GraficalComponent } from './pages/grafical/grafical.component';
-import { NotpagefoundComponent } from './pages/notpagefound/notpagefound.component';
-import { PagesComponent } from './pages/pages.component';
-import { ProgressComponent } from './pages/progress/progress.component';
+// Modulos
+import { PagesRoutingModule } from './pages/pages.routing';
+import { AuthRoutingModule } from './auth/auth.routing';
+
+import { NotpagefoundComponent } from './notpagefound/notpagefound.component';
+
+
 
 const routes: Routes = [
-  { 
-    path: '', 
-    component: PagesComponent,
-    children: [
-      { path: 'dashboard', component: DashboardComponent},
-      { path: 'grafical', component: GraficalComponent},
-      { path: 'progress', component: ProgressComponent},
-      { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
-    ]
-  },
 
-  { path: 'login', component: LoginComponent},
-  { path: 'register', component: RegisterComponent},
-
+  //path: '/dashboard' PageRouting
+  //path: '/login' AuthRouting
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: '**', component: NotpagefoundComponent},
-
 ];
 
 @NgModule({
-  declarations: [],
-  imports: [ RouterModule.forRoot( routes )],
+  imports: [ 
+    RouterModule.forRoot( routes ),
+    PagesRoutingModule,
+    AuthRoutingModule
+  ],
   exports: [ RouterModule ]
 })
 export class AppRoutingModule { }
---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {} from '@angular/router'
+
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { GraficalComponent } from './grafical/grafical.component';
+import { PagesComponent } from './pages.component';
+import { SharedModule } from '../shared/shared.module';
+import { RouterOutlet } from '@angular/router';
+
+
+
+@NgModule({
+  declarations: [
+    DashboardComponent,
+    ProgressComponent,
+    GraficalComponent,
+    PagesComponent,
+  ],
+  exports: [
+    DashboardComponent,
+    ProgressComponent,
+    GraficalComponent,
+    PagesComponent,
+  ],
+  imports: [
+    CommonModule,
+    SharedModule,
+    RouterOutlet
+  ]
+})
+export class PagesModule { }
